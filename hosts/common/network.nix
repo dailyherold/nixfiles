e0d140544a7b8e31e10933f73e62669cebff94b2
dailyherold <git@dailyherold.simplelogin.com>
2025-01-12 16:44:20 -0600

    Install tailscale
    tis awesome

--- hosts/common/network.nix
+++ hosts/common/network.nix
@@ -5,7 +5,19 @@
   # use resolved for hostname resolution
   services.resolved.enable = true;
 
+  # Tailscale
+  services.tailscale.enable = true;
+  # Exit node support: https://tailscale.com/kb/1408/quick-guide-exit-nodes
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
   # enable mdns resolution for resolved on all connections
   # see https://man.archlinux.org/man/NetworkManager.conf.5#CONNECTION_SECTION
   networking.networkmanager.connectionConfig."connection.mdns" = 2;
+
+  environment.systemPackages = [
+    pkgs.tailscale
+  ];
 }
