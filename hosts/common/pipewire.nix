1e26276b30f4f07c6d041e53ed857437969ee691
dailyherold <git@dailyherold.simplelogin.com>
2026-01-27 09:54:29 -0600

    Remove snapcast/client
    - Will move to sendspin in future

--- hosts/common/pipewire.nix
+++ hosts/common/pipewire.nix
@@ -17,19 +17,6 @@
     pkgs.pavucontrol #  GTK gui for controlling audio i/o and volumes
     pkgs.qpwgraph # Qt patchbay for pipewire
     pkgs.helvum # GTK patchbay for pipewire
-    pkgs.snapcast # Used for whome home audio streaming
   ];
 
-  # Add host as a client to any snapserver on LAN
-  systemd.user.services.snapclient-local = {
-    wantedBy = [
-      "pipewire.service"
-    ];
-    after = [
-      "pipewire.service"
-    ];
-    serviceConfig = {
-      ExecStart = "${pkgs.snapcast}/bin/snapclient --player pipewire";
-    };
-  };
 }
